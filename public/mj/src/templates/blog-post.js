5a6cd070027d0f0216ed5de01482e7b1678dc982
Mehdi <80045406+mehdij87@users.noreply.github.com>
2022-01-20 15:46:36 +0300

    cover and post css updated

--- public/mj/src/templates/blog-post.js
+++ public/mj/src/templates/blog-post.js
@@ -83,7 +83,11 @@ export const pageQuery = graphql`
         tags
         img {
           childImageSharp {
-            gatsbyImageData(placeholder: BLURRED, layout: FULL_WIDTH, formats: [AUTO, AVIF, WEBP])
+            gatsbyImageData(
+              placeholder: BLURRED
+              layout: FULL_WIDTH
+              formats: [AUTO, AVIF, WEBP]
+            )
           }
         }
       }
